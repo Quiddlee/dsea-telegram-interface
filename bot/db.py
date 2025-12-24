9f7170cf893d05462111a87c7da83f5f641fb121
Bohdan Shcherbyna <bogdanscherbinadev@gmail.com>
2025-12-24 13:20:16 +0200

    refactor: adjust db ssl mode

--- bot/db.py
+++ bot/db.py
@@ -12,7 +12,7 @@ def get_connection():
     return psycopg2.connect(
         dbname=DB_NAME, user=DB_USER,
         password=DB_PASSWORD, host=DB_HOST, port=DB_PORT,
-        sslmode='require'
+        sslmode='prefer'
     )
 
 
